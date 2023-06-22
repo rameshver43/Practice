8d163eb33d13202b915f210cfdbb9a1d8a257cf6
Rameshver <44090526+rameshver43@users.noreply.github.com>
2023-06-22 23:12:45 +0530

    Update app.py
    added links

--- app.py
+++ app.py
@@ -34,3 +34,37 @@ def transcribe_audio(audio_file):
 
 if __name__ == '__main__':
     app.run()
+
+
+
+
+
+
+Dear All
+w.r.t to user query request classification and what should exactly be included in the program schedule list for the demo tomorrow
+
+Here it is
+
+Just a small tweak to what we discussed on how the request has to be classified.
+
+User Request: includes keywords Affirmations, Gratitude --> Category (video)
+e.g. https://www.youtube.com/watch?v=yo1pJ_D-H3M
+
+
+User Request: includes meditation with sadhguru --> Category (video)
+e.g. https://www.youtube.com/watch?v=PsKEgrx0Xwc
+
+User Request: includes yoga with sadhguru --> category (video)
+
+e.g. https://www.youtube.com/watch?v=EVb7mE9VtW8
+
+User Request: includes Biography   --> Category wiki with image (nltk request)
+e.g. https://en.wikipedia.org/wiki/Mahatma_Gandhi
+output summarized biography with Q&A capability (nltk)
+
+User Request: includes News --> Category (News)
+e.g. https://www.wionews.com/videos
+open this website in browser view
+
+
+Make the search query refined internally to get one link (this exact link) for each of the user request as above
